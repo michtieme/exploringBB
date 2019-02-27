a0aadfaee0588da574fcb21b1d94d9d133782c35
Paul Leslie <paul.leslie@navico.com>
2019-02-28 11:28:06 +1300

    Updated the helloworld to incorporate tr() etc

--- extras/cmake/helloworld/helloworld.cpp
+++ extras/cmake/helloworld/helloworld.cpp
@@ -1,6 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
+void printString(const std::string& stringToPrint)
+{
+
+        std::cout << stringToPrint << "\n";
+}
+
+const char* tr(const char* stringToTranslate)
+{
+        printString("Translated ");
+        return stringToTranslate;
+}
+
+void printSomeFunc()
+{
+        std::string myString(tr("Some String"));
+        std::string myStringA{"Some String A"};
+        std::string myStringB = "Some string B";
+
+        printString(myString);
+        printString(myStringA);
+        printString(myStringB);
+}
 
 int main(int argc, char *argv[]){
+   printSomeFunc();
    std::cout << "Hello World!" << std::endl;
    return 0;
 }
